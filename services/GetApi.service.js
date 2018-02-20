a63c8df69090e6cf150a717ba19815d2b9fa4bcb
doyle928 <doyle928@yahoo.com>
2018-02-20 16:08:36 -0500

    style

--- services/GetApi.service.js
+++ services/GetApi.service.js
@@ -1,19 +1,31 @@
 (function() {
-  function GetApiService() {
-    // var  = {};
-    return {
-      get: get,
-      set: set
+    function GetApi($http) {
+      var weatherData = {};
+    
+      return {
+        returnResults: returnResults,
+        searchWeather: searchWeather
+      };
+      
+      function searchWeather(searchCriteria) {
+        return $http({
+          method: 'GET',
+          url: 'http://api.wunderground.com/api/6223b2f5321ee9e1/conditions/q/' + searchCriteria.state + '/' + searchCriteria.city + '.json'
+        }).then(function successfulCallback(response) {
+          finalData.temp = response.data.current_observation.temp_f;
+          finalData.weather = response.data.current_observation.weather;
+          finalData.feel = response.data.current_observation.feelslike_f;
+        }, function(error) {
+          console.log(error);
+        });
+      }
+      
+      function returnResults() {
+        return finalData;
+      }
     }
-    function get() {
-      // return ;
-    }
-    function set() {
-
-
-    }
-  }
-  angular
-    .module("app")
-    .factory("GetApiService", GetApiService);
-})();
+    
+    angular
+      .module("weatherModule")
+      .factory("WeatherFactory", WeatherFactory);
+    })();
